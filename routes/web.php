240357d7124c8b85eeb30115abead8ef5b5b85e1
Tushar Joshi <tusharvjoshi@gmail.com>
2023-11-15 19:48:08 +0530

    Add routes and empty views

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view("/", 'home')->name('home');
+Route::view("/about", 'about')->name('about');
+Route::view("/history", 'history')->name('history');
+Route::view("/books", 'books')->name('books');
+Route::view("/contact", 'contact')->name('contact');
